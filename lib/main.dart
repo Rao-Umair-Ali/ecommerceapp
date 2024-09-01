3ad0075bac45946ef025bda0d7f39c8544966cc6
Rao-Umair-Ali <ruarua682@gmail.com>
2024-09-01 13:34:59 +0500

    assignment 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return (const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(child: Text("rao")),
-      ),
+          body: Center(
+              child: Text(
+                  "Rao Umair Ali,Flutter developer,Reading quran,teacher"))),
     ));
   }
 }
