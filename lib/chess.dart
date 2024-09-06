d865c2aa9b65ddc72a9989d330dae6e119200079
Rao-Umair-Ali <ruarua682@gmail.com>
2024-09-06 17:54:02 +0500

    structure completed

---
+++ lib/chess.dart
@@ -0,0 +1,465 @@
+import 'package:flutter/material.dart';
+
+class ChessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Chess',
+              style: TextStyle(
+                color: Color.fromARGB(255, 221, 205, 136),
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Play',
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Menu',
+              style: TextStyle(
+                color: Color.fromARGB(255, 221, 205, 136),
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: const Color.fromARGB(255, 38, 39, 41),
+      ),
+      body: Container(
+        color: const Color.fromARGB(255, 101, 156, 55),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 236, 217, 131),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 101, 156, 55),
+                  child: const Text(""),
+                ),
+                Container(
+                  height: 147,
+                  width: 106.625,
+                  color: const Color.fromARGB(255, 221, 205, 136),
+                  child: const Text(""),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
