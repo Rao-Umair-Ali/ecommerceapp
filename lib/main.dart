342d8ba98491bcc837fbe94db19d41996512b39f
Rao-Umair-Ali <ruarua682@gmail.com>
2024-09-06 14:53:22 +0500

    chess completed almost

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,436 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          title: const Center(
+            child: Text("Chess"),
+          ),
           backgroundColor: Colors.blueAccent,
         ),
-        body: Center(child: Text("hello")),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.blueGrey,
+        body: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.black,
+                    child: const Text(""),
+                  ),
+                  Container(
+                    height: 152.5,
+                    width: 106.625,
+                    color: Colors.white,
+                    child: const Text(""),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
